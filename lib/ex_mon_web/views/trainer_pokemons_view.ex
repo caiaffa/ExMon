a1b14501d9cee6e7bf159789790fd9e3dae08371
Luis Santos <luis.santos@pravaler.com.br>
2020-10-02 11:48:27 -0300

    feat: initial commit

---
+++ lib/ex_mon_web/views/trainer_pokemons_view.ex
@@ -0,0 +1,80 @@
+defmodule ExMonWeb.TrainerPokemonsView do
+  use ExMonWeb, :view
+
+  alias ExMon.Trainer.Pokemon
+
+  def render("create.json", %{
+      pokemon: %Pokemon{
+        id: id,
+        name: name,
+        inserted_at: inserted_at,
+        nickname: nickname,
+        types: types,
+        trainer_id: trainer_id,
+        weight: weight
+        }
+      }) do
+    %{
+      message: "Pokemon created!",
+      pokemon: %{
+        id: id,
+        name: name,
+        nickname: nickname,
+        types: types,
+        trainer_id: trainer_id,
+        weight: weight,
+        inserted_at: inserted_at
+      }
+    }
+  end
+
+  def render("show.json", %{
+    pokemon: %Pokemon{
+      id: id,
+      name: name,
+      inserted_at: inserted_at,
+      nickname: nickname,
+      types: types,
+      trainer: %{id: trainer_id, name: trainer_name},
+      weight: weight
+      }
+    }) do
+    %{
+      pokemon: %{
+        id: id,
+        name: name,
+        nickname: nickname,
+        types: types,
+        trainer_id: %{id: trainer_id, name: trainer_name},
+        weight: weight,
+        inserted_at: inserted_at
+      }
+    }
+  end
+
+def render("create.json", %{
+  pokemon: %Pokemon{
+    id: id,
+    name: name,
+    inserted_at: inserted_at,
+    nickname: nickname,
+    types: types,
+    trainer_id: trainer_id,
+    weight: weight
+    }
+  }) do
+  %{
+    message: "Pokemon updated!",
+    pokemon: %{
+      id: id,
+      name: name,
+      nickname: nickname,
+      types: types,
+      trainer_id: trainer_id,
+      weight: weight,
+      inserted_at: inserted_at
+    }
+  }
+  end
+
+end
---
+++ test/ex_mon/trainer/create_test.exs
@@ -0,0 +1,31 @@
+defmodule ExMon.Trainer.CreateTest do
+  use ExMon.DataCase
+
+  alias ExMon.{Repo, Trainer}
+  alias Trainer.Create
+
+  describe "call/1" do
+    test "when all params are valid, creates a trainer" do
+      params = %{name: "Rafael", password: "123456"}
+
+      count_before = Repo.aggregate(Trainer, :count)
+
+      response = Create.call(params)
+
+      count_after = Repo.aggregate(Trainer, :count)
+
+      assert {:ok, %Trainer{name: "Rafael"}} = response
+      assert count_after > count_before
+    end
+
+    test "when there are invalid params, returns the error" do
+      params = %{password: "123456"}
+
+      response = Create.call(params)
+
+      assert {:error, changeset} = response
+      assert errors_on(changeset) == %{name: ["can't be blank"]}
+    end
+  end
+
+end
